a305a8f15fad4c377017152c6886609dbb439475
Ruben <rpierich@hotmail.com>
2017-06-25 19:27:57 -0400

    Fixd bug in writing table to ui.

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class Form1
     Private Function AddButton(name As String, x As Int64, y As Int64)
         Dim newTB2 As New TextBox
         newTB2.Name = name
@@ -6,7 +8,7 @@
         newTB2.Location = New Point(x, y)
         Me.Controls.Add(newTB2)
         newTB2.Text = name
-        Return newTB2.Width
+        Return newTB2
     End Function
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -17,21 +19,42 @@
         Dim tableSeparatorText = Me.tableSeparatorTextBox.Text
 
         Dim getListOfKeywordskeywordList As Hashtable = TemplateParserUtilitiy.ParseHashTableOfElements(input, fieldSeparatorText, tableSeparatorText)
+        Dim documentStructure As List(Of String) = getListOfKeywordskeywordList.Item("documentstructure")
 
-        Dim str = ""
         Dim x = 50
-        For Each de As DictionaryEntry In getListOfKeywordskeywordList
-            If de.Key.ToString().StartsWith("table") Then
-                Dim tableRowList As List(Of TableRow) = de.Value
+        Dim y As Integer = 200
+        For Each dEl As String In documentStructure
+            If dEl.StartsWith("table") Then
+                MsgBox("Found table:  " & dEl)
+                Dim str = ""
+                Dim textBoxHeight = 0
+                For Each de As DictionaryEntry In getListOfKeywordskeywordList
+                    If de.Key.ToString().Equals(dEl) Then
+                        Dim tableRowList As List(Of TableRow) = de.Value
+                        Dim x2 = 50
 
-                For Each ent As TableRow In tableRowList
-                    For Each e2 As String In ent.TemplateFields
-                        Dim Width = Me.AddButton(e2, x, 230)
-                        x += Width
-                    Next
-                Next
+                        For Each ent As TableRow In tableRowList
+                            For Each e2 As String In ent.TemplateFields
+                                Dim but As TextBox = Me.AddButton(e2, x2, y)
+                                textBoxHeight = but.Height
+                                x2 = x2 + but.Width
+                            Next
+                        Next
+                    End If
+                Next de
+                y = y + textBoxHeight
+            Else
+                Dim newTL As TextBox = New TextBox()
+                newTL.Text = dEl
+                newTL.Width = 300
+                newTL.Location = New Point(10, y)
+                y = y + newTL.Height
+                Me.Controls.Add(newTL)
             End If
-        Next de
+
+        Next
+
+
 
         Me.TextBox1.Text = input
         Me.TextBox1.ScrollBars = ScrollBars.Vertical
