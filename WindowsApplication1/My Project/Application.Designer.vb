5cacda07dc520bca3c122abe20d4e338e96420de
Ruben <rpierich@hotmail.com>
2017-06-25 20:27:48 -0400

    Added support for datatypes.

--- WindowsApplication1/My Project/Application.Designer.vb
+++ WindowsApplication1/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.WindowsApplication1.Form1
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.WindowsApplication1.SplashScreen1
+        End Sub
     End Class
 End Namespace
