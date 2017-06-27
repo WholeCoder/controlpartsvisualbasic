ef86de2611ead7dd81e549048a1c8ea0b799fcc9
Ruben <rpierich@hotmail.com>
2017-06-27 05:46:29 -0400

    Tweaked input template.

--- WindowsApplication1/Form2.vb
+++ WindowsApplication1/Form2.vb
@@ -11,4 +11,8 @@
     Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
         Me.Close()
     End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
