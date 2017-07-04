2edd7d2d74bc93863326801907211173bfb9cdfd
Ruben <rpierich@hotmail.com>
2017-07-03 23:37:24 -0400

    Got connection to database to work.

--- ConsoleTesting/Module1.vb
+++ ConsoleTesting/Module1.vb
@@ -1,20 +1,26 @@
-﻿Imports System.Text.RegularExpressions
+﻿Imports System.Data.SqlClient
+Imports System.Runtime.Remoting.Channels
+Imports System.Text.RegularExpressions
 Imports WindowsApplication1
+Imports System.Data
 
 Module Module1
-     Sub Main()
-        Dim preBreakUpTableRow As String = "<tr><td>%column:partnumber%</td><td>%column:cost%</td><td>%column:voltage%</td></tr>"
+    Sub Main()
 
-        Dim parsedFieldsIntoHashTable As Hashtable = TemplateParserUtilitiy.ParseHashTableOfElements(preBreakUpTableRow, "%", "NotUsed", "NotUsed")
-        Console.WriteLine(parsedFieldsIntoHashTable.Count)
+        Dim objConnection As SqlConnection = New SqlConnection("Server=localhost" & "\SQLEXPRESS;Database=ControlParts;" &
+                                                               "User ID=sa;Password=ssGood&Plenty;")
+        ' Open the database connection.
+        objConnection.Open() ' .. Use the connection' Close the database
 
-        For Each Item In parsedFieldsIntoHashTable
-            If Item.Key.Equals("documentstructure") Then
-                Console.WriteLine("got document structure")
-            Else
-                Console.WriteLine("Type:  " & Item.Value)
-            End If
-        Next
-        Console.ReadKey()
+        Dim objCommand As SqlCommand = New SqlCommand()
+        objCommand.Connection = objConnection
+        objCommand.CommandText = "INSERT INTO authors " & "(au_lname, au_fname, contract) " &
+                                                    "VALUES('Barnes', 'David', 1)"
+        objCommand.ExecuteNonQuery()
+
+        objConnection.Close()
+
+        Console.WriteLine("Press any key to continue.")
+        Console.ReadLine()
     End Sub
 End Module
