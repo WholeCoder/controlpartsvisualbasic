7febdd133d2d560fdea5a261cf533f28f09fe38b
Ruben <rpierich@hotmail.com>
2017-07-06 01:31:47 -0400

    Figured out how to tell if table exists and query it.

--- ConsoleTesting/Module1.vb
+++ ConsoleTesting/Module1.vb
@@ -1,26 +1,65 @@
-﻿Imports System.Data.SqlClient
-Imports System.Runtime.Remoting.Channels
-Imports System.Text.RegularExpressions
-Imports WindowsApplication1
+﻿
+Option Explicit On
+Option Strict On
+
 Imports System.Data
+Imports System.Data.SqlClient
 
 Module Module1
+
     Sub Main()
+        Dim str As String = "Server = localhost" & "\SQLEXPRESS;Database=ControlParts;" &
+                                      "User ID=sa;Password=ssGood&Plenty;"
+        ReadOrderData(str)
 
-        Dim objConnection As SqlConnection = New SqlConnection("Server=localhost" & "\SQLEXPRESS;Database=ControlParts;" &
-                                                               "User ID=sa;Password=ssGood&Plenty;")
-        ' Open the database connection.
-        objConnection.Open() ' .. Use the connection' Close the database
-
-        Dim objCommand As SqlCommand = New SqlCommand()
-        objCommand.Connection = objConnection
-        objCommand.CommandText = "INSERT INTO authors " & "(au_lname, au_fname, contract) " &
-                                                    "VALUES('Barnes', 'David', 1)"
-        objCommand.ExecuteNonQuery()
-
-        objConnection.Close()
-
-        Console.WriteLine("Press any key to continue.")
-        Console.ReadLine()
     End Sub
+
+    Private Sub ReadOrderData(ByVal connectionString As String)
+        Dim queryString As String =
+            "Select * From sys.tables Where name = 'authors' AND type = 'U';"
+        Dim createTableString = "CREATE TABLE " & "ControlParts" & ". People (" &
+                                         "Id int NOT NULL PRIMARY KEY, " &
+                                         "LastName  VARCHAR(30), " &
+                                         "FirstName VARCHAR(20), " &
+                                         "Address   VARCHAR(50) " &
+                                         ") "
+
+        Using connection As New SqlConnection(connectionString)
+            Dim command As New SqlCommand(queryString, connection)
+            connection.Open()
+
+            Dim reader As SqlDataReader = command.ExecuteReader()
+
+            ' Call Read before accessing data.
+            While reader.Read()
+                ReadSingleRow(CType(reader, IDataRecord))
+            End While
+
+            Dim obj As SqlCommand
+            Dim strSQL As String
+            obj = connection.CreateCommand()
+            strSQL = "CREATE TABLE " & "ControlParts" & "  (" &
+                     "Id int NOT NULL PRIMARY KEY, " &
+                     "LastName  VARCHAR(30), " &
+                     "FirstName VARCHAR(20), " &
+                     "Address   VARCHAR(50) " &
+                     ") "
+            ' Execute
+            ' Call Close when done reading.
+            reader.Close()
+
+            obj.CommandText = strSQL
+            obj.ExecuteNonQuery()
+            connection.Close()
+
+            Console.WriteLine("Pres enter to continue")
+            Console.ReadLine()
+        End Using
+    End Sub
+
+    Private Sub ReadSingleRow(ByVal record As IDataRecord)
+        Console.WriteLine(String.Format("{0}", record(0), record(1)))
+
+    End Sub
+
 End Module
