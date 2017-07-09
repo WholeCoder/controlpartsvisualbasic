b644d3598f2e58fc7bdebc535f163ec5c3046f6f
Ruben <rpierich@hotmail.com>
2017-07-09 11:14:00 -0400

    Added a test for ControlPartsTest database.

--- ConsoleTesting/Module1.vb
+++ ConsoleTesting/Module1.vb
@@ -11,9 +11,28 @@ Module Module1
         '        Dim str As String = "Server = localhost" & "\SQLEXPRESS;Database=ControlParts;" &
         '                                      "User ID=sa;Password=ssGood&Plenty;"
         '        ReadOrderData(str)
+        Dim queryString As String =
+                "Select * from templates Where name = 'arr12';"
+        Dim connectionString As String = "Server = localhost" & "\SQLEXPRESS;Database=ControlPartsTest;" & "User ID=sa;Password=ssGood&Plenty;"
 
-        Dim str As String = "table:rubenstale:straing"
-        Console.WriteLine(Split(str, ":")(1))
+        Using connection As New SqlConnection(connectionString)
+            Dim command As New SqlCommand(queryString, connection)
+            connection.Open()
+
+            Dim reader As SqlDataReader = command.ExecuteReader()
+
+            ' Call Read before accessing data.
+            Dim tableName As String = ""
+            While reader.Read()
+                tableName = ReadSingleRow(CType(reader, IDataRecord))
+                Console.WriteLine(tableName)
+            End While
+            reader.Close()
+
+            connection.Close()
+            Console.WriteLine(tableName.Equals("arr12"))
+        End Using
+
         Console.ReadLine()
     End Sub
 
@@ -60,9 +79,9 @@ Module Module1
         End Using
     End Sub
 
-    Private Sub ReadSingleRow(ByVal record As IDataRecord)
-        Console.WriteLine(String.Format("{0}", record(0), record(1)))
+    Private Function ReadSingleRow(ByVal record As IDataRecord) As String
+        Return (String.Format("{0}", record(0), record(1)))
 
-    End Sub
+    End Function
 
 End Module
---
+++ TestControlParts/TestDatabase.vb
@@ -0,0 +1,39 @@
+﻿Imports System.CodeDom.Compiler
+Imports System.Data.SqlClient
+Imports WindowsApplication1
+Imports NUnit.Framework
+
+Public Class TestDatabase
+    <Test()>
+    Public Sub testTemplateExists()
+        '        Dim str As String = "Server = localhost" & "\SQLEXPRESS;Database=ControlParts;" &
+        '                                      "User ID=sa;Password=ssGood&Plenty;"
+        '        ReadOrderData(str)
+        Dim queryString As String =
+                "Select * from templates Where name = 'arr12';"
+        Dim connectionString As String = "Server = localhost" & "\SQLEXPRESS;Database=ControlPartsTest;" & "User ID=sa;Password=ssGood&Plenty;"
+        Dim tableName As String = ""
+        Using connection As New SqlConnection(connectionString)
+            Dim command As New SqlCommand(queryString, connection)
+            connection.Open()
+
+            Dim reader As SqlDataReader = command.ExecuteReader()
+
+            ' Call Read before accessing data.
+            While reader.Read()
+                tableName = ReadSingleRow(CType(reader, IDataRecord))
+                Console.WriteLine(tableName)
+            End While
+            reader.Close()
+
+            connection.Close()
+        End Using
+        Assert.True(tableName.Equals("arr12"))
+    End Sub
+
+    Private Function ReadSingleRow(ByVal record As IDataRecord) As String
+        Return String.Format("{0}", record(0), record(1))
+
+    End Function
+
+End Class
