7df5dafa12767e315cbde20c03eff59662beb83c
Ruben <rpierich@hotmail.com>
2017-08-27 03:37:03 -0400

    Almost done with conversion to wpf app from windows forms app.

--- WpfControlPartsApplication/Form1.xaml.vb
+++ WpfControlPartsApplication/Form1.xaml.vb
@@ -5,6 +5,31 @@ Public Class Form1
     Public myObjectSavers As List(Of SaveOrLoadFromToDatabaseObject)
     Public myHTMLObjectSavers As List(Of SaveOrLoadFromToDatabaseObject)
 
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        For Each oSaver As SaveOrLoadFromToDatabaseObject In myObjectSavers
+            oSaver.SaveToDatabase()
+        Next
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        For Each oSaver As SaveOrLoadFromToDatabaseObject In myObjectSavers
+            oSaver.LoadFromDatabase()
+        Next
+
+        Dim htmlString As String = ""
+
+        For Each oSaver As SaveOrLoadFromToDatabaseObject In myHTMLObjectSavers
+            htmlString &= oSaver.ConvertToHTMLDocument() & "                   "
+        Next
+        My.Windows.ViewHTMLWindow.HTMLTextBox.Text = htmlString
+        My.Windows.ViewHTMLWindow.Show()
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+        For Each oSaver As SaveOrLoadFromToDatabaseObject In myObjectSavers
+            oSaver.LoadFromDatabase()
+        Next
+    End Sub
 End Class
 
 
